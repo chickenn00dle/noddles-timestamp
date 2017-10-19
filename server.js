9bdede051d8452d0e223cb203685f0e7ff6908ce
Glitch (noodles-timestamp) <none>
2017-10-19 04:09:42 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -2,6 +2,43 @@
 // where your node app starts
 
 // init project
-var http = require('http');
+var express = require('express');
+var moment = require('moment');
+var port = 8000;
 
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/:date', (req, res) => {
+  if (req.params) {
+    var param = req.params.date;
+    if (moment(param).isValid()){
+      var unix = moment(param).unix();
+      var natural = moment(param).format('MMMM D, YYYY');
+    } else {
+      var unix = null;
+      var natural = null;
+    }
+    
+    res.send({
+      "unix": unix,
+      "natural": natural
+    });
+    
+  }
+});
+
+app.put("/:date", (req, res) => {
+  console.log(req.params);
+});
+
+var listener = app.listen(process.env.PORT, (err, res) => {
+  if (err) throw err;
+  console.log('Your app is listening on port ' + listener.address().port);
+});
 
